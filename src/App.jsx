38dc52be3ac7c07ded542d170a709e26f322e3c2
cuziam <yameame320@gmail.com>
2024-01-08 17:28:32 +0900

    minor fix: Translate button 위치를 올바르게 변경하였음

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 
 //user-defined components
-import TranslateResults from "./components/TranslateResults";
-import TranslateSource from "./components/TranslateSource";
+import Translate from "./components/Translate";
 function App() {
   return (
     <>
-      <div className="Translate w-96 p-2 flex-col justify-center items-center gap-2 flex">
-        <TranslateSource />
-        <TranslateResults />
-      </div>
+      <Translate />
     </>
   );
 }
--- src/components/SourceToolbar.jsx
+++ src/components/SourceToolbar.jsx
@@ -4,12 +4,9 @@ import Tools from "./Tools";
 
 function SourceToolbar() {
   return (
-    <div className="SourceToolbar w-80 h-9 pl-2 py-2 bg-background rounded-bl-md rounded-br-md border-2 border-black border-opacity-5 justify-between items-center inline-flex">
+    <div className="SourceToolbar w-80 h-9 pl-2 py-2 bg-background rounded-bl-md rounded-br-md border-2 border-black border-opacity-5 flex justify-between items-center">
       <Tools />
-      <div
-        className="BtnTranslate w-28 h-9 p-2 bg-secondary hover:bg-secondaryGray rounded-br-lg border-2 border-zinc-300 justify-between items-center
-      text-white font-bold cursor-pointer text-sm text-center"
-      >
+      <div className="BtnTranslate flex items-center justify-center w-28 h-9 bg-secondary hover:bg-secondaryGray rounded-br-lg border-2 border-zinc-300 text-white text-sm font-bold cursor-pointer">
         Translate
       </div>
     </div>
