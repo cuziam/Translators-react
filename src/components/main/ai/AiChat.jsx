819744516e8750cd6b09e4b12a8df8cf86097d91
cuziam <yameame320@gmail.com>
2024-01-29 15:13:28 +0900

    history와 aichat에 대한 다크모드

--- src/components/main/ai/AiChat.jsx
+++ src/components/main/ai/AiChat.jsx
@@ -111,13 +111,13 @@ const AiChat = ({ webSocketRef, shouldAiChatOpen, updateShouldAiChatOpen }) => {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden max-h-screen rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="bg-white px-4 pt-2 sm:p-6 sm:pb-4">
+              <Dialog.Panel className="relative transform overflow-hidden max-h-screen rounded-lg  bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white dark:bg-disabled text-white px-4 pt-2 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                       <Dialog.Title
                         as="h3"
-                        className="text-base align-middle content-center font-bold leading-10 text-gray-900"
+                        className="text-base align-middle content-center font-bold leading-10 text-gray-900 dark:text-white"
                       >
                         Ask to AI assistant
                       </Dialog.Title>
@@ -164,7 +164,7 @@ const AiChat = ({ webSocketRef, shouldAiChatOpen, updateShouldAiChatOpen }) => {
                     </div>
                   </div>
                 </div>
-                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <div className="bg-gray-50 dark:bg-disabled px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                   <button
                     type="button"
                     className="cancel inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
--- src/components/main/history/HistoryModal.jsx
+++ src/components/main/history/HistoryModal.jsx
@@ -47,12 +47,12 @@ export default function HistoryModal({
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                <div className="bg-white  dark:bg-disabled px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                       <Dialog.Title
                         as="h3"
-                        className="text-base font-semibold leading-6 text-gray-900"
+                        className="text-base font-semibold leading-6 text-gray-900 dark:text-white"
                       >
                         Your history
                       </Dialog.Title>
@@ -79,7 +79,7 @@ export default function HistoryModal({
                     </div>
                   </div>
                 </div>
-                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <div className="bg-gray-50 dark:bg-disabled px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                   <button
                     type="button"
                     className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
