529f733b1e9520ad2cadffbc8de1986b56d89036
cuziam <yameame320@gmail.com>
2024-02-01 10:20:30 +0900

    기존의 express서버 코드 및 파일들을 현재 프로젝트에 전부 병합하였음

--- src/components/main/TargetToolbar.jsx
+++
@@ -1,9 +0,0 @@
-import Tools from "./Tools";
-
-export default function TargetToolbar() {
-  return (
-    <div className="Targettoolbar w-80 h-9 p-2 bg-background rounded-bl-md rounded-br-md border-2 border-black border-opacity-5 justify-start items-center inline-flex">
-      <Tools />
-    </div>
-  );
-}
