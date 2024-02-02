4da5374548e8e28bb9c73e505806ca88befe886c
cuziam <yameame320@gmail.com>
2024-02-02 19:26:28 +0900

    마이크 ui추가

--- src/client/components/main/Tools.jsx
+++ src/client/components/main/Tools.jsx
@@ -47,6 +47,37 @@ function Tools() {
       {context === useContext(SourceContext) ? (
         <>
           <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            className="group w-6 h-6 fill-icon"
+            version="1.1"
+            viewBox="0 0 14 21"
+          >
+            <title />
+            <desc />
+            <defs />
+            <g
+              fill="none"
+              fillRule="evenodd"
+              id="Page-1"
+              stroke="none"
+              strokeWidth="1"
+            >
+              <g
+                className="fill-icon group-active:fill-primary"
+                id="Icons-AV"
+                transform="translate(-3.000000, -43.000000)"
+              >
+                <g id="mic" transform="translate(3.000000, 43.500000)">
+                  <path
+                    d="M7,12 C8.7,12 10,10.7 10,9 L10,3 C10,1.3 8.7,0 7,0 C5.3,0 4,1.3 4,3 L4,9 C4,10.7 5.3,12 7,12 L7,12 Z M12.3,9 C12.3,12 9.8,14.1 7,14.1 C4.2,14.1 1.7,12 1.7,9 L0,9 C0,12.4 2.7,15.2 6,15.7 L6,19 L8,19 L8,15.7 C11.3,15.2 14,12.4 14,9 L12.3,9 L12.3,9 Z"
+                    id="Shape"
+                  />
+                </g>
+              </g>
+            </g>
+          </svg>
+          <svg
             viewBox="0 0 31 30"
             xmlns="http://www.w3.org/2000/svg"
             className="history group w-6 h-6 fill-icon"
