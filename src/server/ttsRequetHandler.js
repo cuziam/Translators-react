e72013da1f1fb1c092014ada6a41c60deb3a485e
cuziam <yameame320@gmail.com>
2024-02-18 13:57:43 +0900

    변수명 수정 opus=>aac

--- src/server/ttsRequetHandler.js
+++ src/server/ttsRequetHandler.js
@@ -18,20 +18,21 @@ async function handleTtsRequest(socket) {
     console.log("클라이언트 TTS 요청을 받았습니다.", message);
     //TTS 로직 처리
     try {
-      const opus = await openai.audio.speech.create({
+      const aac = await openai.audio.speech.create({
         model: "tts-1",
         voice: "nova",
         response_format: "aac",
         input: message,
       });
 
-      const buffer = Buffer.from(await opus.arrayBuffer());
+      const buffer = Buffer.from(await aac.arrayBuffer());
       const fileName = `speech-${randomUUID()}.aac`; //고유한 파일명 생성
       const speechFilePath = path.resolve(`./src/server/voices/${fileName}`);
       await fs.promises.writeFile(speechFilePath, buffer); //파일 저장
       console.log("TTS 파일을 생성하였습니다.");
       //클라이언트에게 파일 URL 전송
       const fileUrl = `${process.env.APP_URL}/voices/${fileName}`; //서버 유알엘이 로컬호스트가 아닌 실제 서버 주소로 변경되어야함
+      console.log("fileUrl:", fileUrl);
       callback(fileUrl);
     } catch (err) {
       console.error(err);
